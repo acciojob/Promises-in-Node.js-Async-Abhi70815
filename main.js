620ad527c42eabed658ee1e54d5f44917d75b0ba
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-29 11:24:57 +0530

    save

--- main.js
+++ main.js
@@ -17,6 +17,12 @@ function readFileAsync(filePath) {
 function modifyText(text) {
   return new Promise((resolve, reject) => {
     // TODO: Convert text to uppercase and reverse it
+    if(text){
+      resolve(text.toUpperCase().split('').reverse().join(''));
+    }
+    else{
+      reject("invalid text");
+    }
   });
 }
 
